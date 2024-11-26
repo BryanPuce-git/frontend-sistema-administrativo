2e8917667179d57a4bbc88af03f6a7c6a4446051
Bryan Farinango <bifarinango@pucesi.edu.ec>
2024-11-26 13:24:56 -0500

    Feat: Sección idiomas Terminado

--- src/modules/curriculum/componentes/LanguageSection.vue
+++ src/modules/curriculum/componentes/LanguageSection.vue
@@ -24,7 +24,7 @@
 
       <!-- Lista de idiomas seleccionados con opciones para editar -->
       <div v-if="idiomas.length > 0" class="mb-4">
-        <div v-for="idioma in idiomas" :key="idioma.idi_id" class="grid grid-cols-3 gap-4 items-center mb-2">
+        <div v-for="idioma in idiomas" :key="idioma.idi_id" class="grid grid-cols-4 gap-4 items-center mb-2">
           <!-- Selección de idioma -->
           <div>
             <select v-model="idioma.idi_idioma"
@@ -47,16 +47,14 @@
               </option>
             </select>
           </div>
-          <!-- Checkbox para excluyente -->
-          <div>
-            <label class="inline-flex items-center">
-              <input type="checkbox" v-model="idioma.excluyente" class="form-checkbox text-red-500"
-                @change="actualizarIdioma(idioma)" />
-              <span class="ml-2">Excluyente</span>
-            </label>
-          </div>
+          <!-- Botón para eliminar -->
+          <button @click="eliminarIdioma(idioma.idi_id)" class="text-gray-500 hover:text-red-600" type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
         </div>
-
       </div>
 
       <!-- Botón para agregar nuevo idioma -->
@@ -98,21 +96,22 @@
           Guardar Idioma
         </button>
       </div>
+
+      <!-- Campos excluyentes -->
       <div class="mt-4 text-center">
         <button @click="mostrarMasIdioma" type="button" class="text-blue-500 hover:text-blue-700 focus:outline-none">
           Ver más <span v-if="!mostrarDetalleIdioma">▼</span><span v-else>▲</span>
         </button>
       </div>
       <div v-if="mostrarDetalleIdioma" class="mt-4">
-
         <div class="bg-red-100 border border-red-300 p-4 rounded-lg mb-6 mt-4">
           <h4 class="text-red-500 font-bold mb-2">Campos Excluyentes</h4>
-          <p class="text-sm text-black-500 mb-4"><b>Advertencia: </b>selecciona los campos que son indispensables
+          <p class="text-sm text-black-500 mb-4"><b>Advertencia:</b> selecciona los campos que son indispensables
             para el perfil. Ten en cuenta que si un candidato no cumple con un campo excluyente será descartado
             del proceso.</p>
           <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             <label class="inline-flex items-center">
-              <input type="checkbox" v-model="excluyente" class="form-checkbox text-red-500">
+              <input type="checkbox" v-model="todosExcluyentes" @change="actualizarTodosExcluyentes" class="form-checkbox text-red-500">
               <span class="ml-2">Idioma</span>
             </label>
           </div>
@@ -136,7 +135,7 @@ const props = defineProps({
   },
 });
 
-const excluyente = ref(false);
+const todosExcluyentes = ref(false);
 
 const mostrarDetalleIdioma = ref(false);
 
@@ -172,27 +171,30 @@ const obtenerIdiomas = async () => {
       idi_id: item.idi_id,
       idi_idioma: item.idi_idioma,
       idi_nivel: item.idi_nivel,
-      excluyente: item.idi_idioma_excluyente === 1, // Correcto
+      excluyente: item.idi_idioma_excluyente === 1, 
     }));
-    console.log("Idiomas obtenidos:", idiomas.value);
+    todosExcluyentes.value = idiomas.value.every(idioma => idioma.excluyente);
   } catch (error) {
     console.error("Error al obtener los idiomas:", error);
   }
 };
 
-
+// Sincronizar excluyentes
+const actualizarTodosExcluyentes = () => {
+  idiomas.value.forEach(idioma => {
+    idioma.excluyente = todosExcluyentes.value;
+    actualizarIdioma(idioma);
+  });
+};
 
 // Agregar un idioma
 const agregarIdioma = async () => {
-  if (!nuevoIdioma.value.idioma || !nuevoIdioma.value.nivel) {
-    console.warn("Por favor selecciona un idioma y un nivel.");
-    return;
-  }
+  if (!nuevoIdioma.value.idioma || !nuevoIdioma.value.nivel) return;
   const data = {
     pcom_id: props.id,
     idi_idioma: nuevoIdioma.value.idioma,
     idi_nivel: nuevoIdioma.value.nivel,
-    idi_idioma_excluyente: excluyente.value ? 1 : 0,
+    idi_idioma_excluyente: todosExcluyentes.value ? 1 : 0,
   };
   try {
     const response = await useApi.post("/api/v1/curriculum/idiomas", data);
@@ -200,10 +202,9 @@ const agregarIdioma = async () => {
       idi_id: response.data.idi_id,
       idi_idioma: nuevoIdioma.value.idioma,
       idi_nivel: nuevoIdioma.value.nivel,
-      excluyente: excluyente.value,
+      excluyente: todosExcluyentes.value,
     });
     nuevoIdioma.value = { idioma: '', nivel: '' };
-    excluyente.value = false;
     mostrarCampos.value = false;
   } catch (error) {
     console.error("Error al guardar el idioma:", error);
@@ -211,30 +212,25 @@ const agregarIdioma = async () => {
 };
 
 
-
-// Actualizar un idioma
 const actualizarIdioma = async (idioma) => {
   try {
     const data = {
       pcom_id: props.id,
       idi_idioma: idioma.idi_idioma,
       idi_nivel: idioma.idi_nivel,
-      idi_idioma_excluyente: idioma.excluyente ? 1 : 0, // Incluye el campo excluyente
+      idi_idioma_excluyente: idioma.excluyente ? 1 : 0,
     };
     await useApi.patch(`/api/v1/curriculum/idioma/${idioma.idi_id}`, data);
-    console.log(`Idioma con ID ${idioma.idi_id} actualizado correctamente.`);
   } catch (error) {
     console.error(`Error al actualizar el idioma con ID ${idioma.idi_id}:`, error);
   }
 };
-
 
 // Eliminar un idioma
 const eliminarIdioma = async (idi_id) => {
   try {
     await useApi.delete(`/api/v1/curriculum/idioma/${idi_id}`);
     idiomas.value = idiomas.value.filter(idioma => idioma.idi_id !== idi_id);
-    console.log(`Idioma con ID ${idi_id} eliminado correctamente.`);
   } catch (error) {
     console.error(`Error al eliminar el idioma con ID ${idi_id}:`, error);
   }
